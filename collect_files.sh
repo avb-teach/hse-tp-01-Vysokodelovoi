94b26c5604e94ed5480f8ab57d17650f45b7857f
Daniel Khusainov <217623@stud.uust.ru>
2025-04-27 07:26:31 +0300

    Changed max_depth argument place to the end of command

--- collect_files.sh
+++ collect_files.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-if [[ "$1" == "--max_depth" ]]; then
-  MAX_DEPTH="$2"
-  INPUT_DIR="$3"
-  OUTPUT_DIR="$4"
+if [[ "$3" == "--max_depth" ]]; then
+  MAX_DEPTH="$4"
+  INPUT_DIR="$1"
+  OUTPUT_DIR="$2"
 else
   MAX_DEPTH="-1"
   INPUT_DIR="$1"
